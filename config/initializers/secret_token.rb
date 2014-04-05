f30faa960a48d7be6d20eeb193a54cfde02314fd
Yosuke Adachi <front.lightstone@gmail.com>
2014-04-06 00:37:08 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'bfe4207ef939f1b7c0b2f445b20d10a723999d6ea9a447add7d2de21489ca9dfd49e0e2090c24d609d35492fd82b46ccd85e1fc0de73ee9bb62109e217a7ca90'
